a759b6a605f45e52bf4eb7ebf88a5ef4e34bc540
joecoker <joe_coker@hotmail.co.uk>
2018-10-23 14:24:30 +0100

    Refactor into more methods

--- directory.rb
+++ directory.rb
@@ -1,21 +1,22 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To Finish, just hit return twice"
 
-  students = []
   name = gets.chomp
 
   puts "What cohort are they in?"
   cohort = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: cohort}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
-    
+
     puts "What is the next students name?"
     name = gets.chomp
     puts "What cohort are they in?"
@@ -24,11 +25,8 @@ def input_students
     if cohort.length == 0
       cohort = "default"
     end
-
     cohort.to_sym
   end
-
-  students
 end
 
 def print_header
@@ -36,23 +34,23 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_student_list()
   counter = 0
-  while students.length > counter
-    puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+  while @students.length > counter
+    puts "#{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)"
     counter +=1
   end
 end
 
-def print_by_cohort(students)
-  cohort_list = students.map do |student|
+def print_by_cohort
+  cohort_list = @students.map do |student|
     student[:cohort]
   end
   cohort_list = cohort_list.uniq
 
   counter = 0
   while counter < cohort_list.length
-    students.each do |student|
+    @students.each do |student|
       if student[:cohort] == cohort_list[counter]
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
       end
@@ -61,11 +59,41 @@ def print_by_cohort(students)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print_footer(students)
-print_by_cohort(students)
+def interactive_menu
+  students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+interactive_menu
